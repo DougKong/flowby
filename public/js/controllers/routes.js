e6bb298fef20100813f4b8486c3380986ccbab83
Doug Kong <kongdouglas@gmail.com>
2013-09-19 18:53:26 -0700

    change route instructions to use ng-grid

--- public/js/controllers/routes.js
+++ public/js/controllers/routes.js
@@ -1,7 +1,7 @@
 angular.module('mean.routes')
 .controller('RoutesController', 
   ["$scope", function ($scope) {
-    $scope.steps = [
+    $scope.myData = [
       {order: 1, instruction:'turn right', distance:'5mi', duration:'3 minutes'},
       {order: 2, instruction:'turn left', distance:'11mi', duration:'5 minutes'},
       {order: 3, instruction:'turn right', distance:'5mi', duration:'3 minutes'},
@@ -22,4 +22,5 @@ angular.module('mean.routes')
       {order: 18, instruction:'turn left', distance:'11mi', duration:'5 minutes'},
       {order: 19, instruction:'turn right', distance:'28mi', duration:'7 minutes'}
     ];
+  $scope.myOptions = { data: 'myData' };
 }]);
