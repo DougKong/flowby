02b1b7ed1b475d937baed2a2cc9f5b34ab666db1
Doug Kong <kongdouglas@gmail.com>
2013-09-22 21:08:53 -0700

    separate loading of the map from the loading of the markers on the map

--- public/js/controllers/map.js
+++ public/js/controllers/map.js
@@ -40,27 +40,21 @@ angular.module('mean.map')
             longitude: -73
           },
           /** the initial zoom level of the map */
-          zoomProperty: 8,
+          zoomProperty: 8
+        });
+      };
+      $scope.getMarkers = function() {
+        angular.extend($scope, {    
           /** list of markers to put in the map */
           markersProperty: $scope.markers,
-
-          //These 2 properties will be set when clicking on the map
-          clickedLatitudeProperty: null,
-          clickedLongitudeProperty: null,
-          eventsProperty: {
-            click: function (mapModel, eventName, originalEventArgs) {
-              // 'this' is the directive's scope
-              $log.log("user defined event on map directive with scope", this);
-              $log.log("user defined event: " + eventName, mapModel, originalEventArgs);
-            }
-          }
         });
       };
       $scope.getShipments();
+      $scope.getMap();
       deferred.promise.then(
         function() {
-          $scope.getMap();
+          $scope.getMarkers();
       });
-      $scope.getMap();
+
     }]
   );
