7810be9fa520fab8990282cf2ee0b60982a71b6c
Doug Kong <kongdouglas@gmail.com>
2013-09-25 11:44:42 -0700

    display drivers used of total drivers available

--- public/js/controllers/drivers.js
+++ public/js/controllers/drivers.js
@@ -5,5 +5,20 @@ angular.module('mean.drivers')
     { name: 'Dave', telephone: "(348)-238-8982" },
     { name: 'Alice', telephone: "(370)-382-1010" },
     { name: 'Tim', telephone: "(289)-289-2389" }];
-  $scope.myOptions = { data:  'myData'};
+
+  $scope.driversTotal = $scope.myData.length;
+
+  $scope.$on('ngGridEventData', function(){
+    $scope.myOptions.selectRow(1, true);
+  });
+  $scope.mySelections = [];
+  $scope.driversUsed = 0;
+  $scope.myOptions = {
+    data:  'myData',
+    selectedItems: $scope.mySelections,
+    afterSelectionChange: function(data){
+      $scope.driversUsed = $scope.mySelections.length;
+    }
+  };
+
 }]);
