8e9c8c862127cbd69930db26b5d23d8d01720d3e
Doug Kong <kongdouglas@gmail.com>
2013-09-25 10:26:49 -0700

    change the layout and info for the drivers display

--- public/js/controllers/drivers.js
+++ public/js/controllers/drivers.js
@@ -1,9 +1,9 @@
 angular.module('mean.drivers')
 .controller('DriversController', ['$scope', function($scope){
-  //console.log('drivercontroller');
   $scope.myData = [
-    {name: 'Moroni', age: 50},
-    {name: 'Dave', age:48},
-    {name: 'Bob', age:70 }];
+    { name: 'Jim', telephone: "(617)-378-3989" },
+    { name: 'Dave', telephone: "(348)-238-8982" },
+    { name: 'Alice', telephone: "(370)-382-1010" },
+    { name: 'Tim', telephone: "(289)-289-2389" }];
   $scope.myOptions = { data:  'myData'};
 }]);
