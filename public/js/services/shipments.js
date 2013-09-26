eb6d9d12a8b80f3579fd10511ae6ccb6abad87d6
Doug Kong <kongdouglas@gmail.com>
2013-09-26 15:57:57 -0700

    clean up console.log and gantt chart related issues

--- public/js/services/shipments.js
+++ public/js/services/shipments.js
@@ -1,5 +1,4 @@
 angular.module('mean.map').factory("Shipments", ['$resource', function($resource) {
-  console.log('Shipments service');
   return $resource('shipments/:shipmentId', {
     shipmentId: '@_id'
   }, {
